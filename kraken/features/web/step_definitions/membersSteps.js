abd2c8f1f1bdad3bfa145404a2fdde70792ca023
Stevenhr <estevenhr@hotmail.com>
2024-11-09 10:58:41 -0500

    Feat(test): Escenario Crear cuenta en Ghost con correo electronico invalido

--- kraken/features/web/step_definitions/membersSteps.js
+++ kraken/features/web/step_definitions/membersSteps.js
@@ -1,6 +1,7 @@
 const { Given, When, Then } = require('@cucumber/cucumber');
 const MembersPage = require('../page_objects/MembersPage');
-
+const { faker } = require('@faker-js/faker');
+const assert = require('assert');
 
 When('I click Members', async function() {
     const membersPage = new MembersPage(this.driver);
@@ -18,6 +19,12 @@ When('I enter name member {kraken-string}', async function (name) {
 });
 
 When('I enter email member {kraken-string}', async function (email) {
+    const randomEmail = faker.internet.email();
+    const membersPage = new MembersPage(this.driver);
+    await membersPage.enterEmail(randomEmail);
+});
+
+When('I enter emailinvalido member {kraken-string}', async function (email) {
     const membersPage = new MembersPage(this.driver);
     await membersPage.enterEmail(email);
 });
@@ -34,21 +41,27 @@ When('I click save member', async function() {
 
 
 Then('I should see the Created message with the date', async function () {
-    // Espera un momento para que aparezca el mensaje después de guardar
+    
     await this.driver.pause(5000); 
-
-    // Busca el contenedor principal del mensaje "Created"
     const createdContainer = await this.driver.$(".gh-member-details-attribution p");
-
-    // Verifica si el elemento está presente y visible
+    
     const isDisplayed = await createdContainer.isDisplayed();
     if (!isDisplayed) {
         throw new Error("El mensaje 'Created' no está visible en la página.");
     }
 
-    // Opcional: Obtener el texto y validar que contiene "Created"
     const textContent = await createdContainer.getText();
     if (!textContent.includes("Created")) {
         throw new Error("El mensaje 'Created' no se encuentra en el texto del elemento.");
     }
+});
+
+
+Then('I should see the error message "Invalid Email."', async function () {
+    const errorMessageElement = await this.driver.$('.error .response');
+    await errorMessageElement.waitForDisplayed({ timeout: 10000 });
+
+    const errorMessageText = await errorMessageElement.getText();
+    assert.strictEqual(errorMessageText, 'Invalid Email.', 'El mensaje de error no es el esperado.');
+    
 });

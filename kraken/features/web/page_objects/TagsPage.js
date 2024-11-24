4e3add94708953b077b57c3d5da29126663d1afd
Stevenhr <estevenhr@hotmail.com>
2024-11-23 23:30:29 -0500

    Feat(EP-011-DP): Crear tags y Meta Data con datos correctos

--- kraken/features/web/page_objects/TagsPage.js
+++ kraken/features/web/page_objects/TagsPage.js
@@ -85,9 +85,9 @@ class TagsPage {
         }
         
         
-      }
+    }
 
-      async getMensaje(mensaje) {
+    async getMensaje(mensaje) {
         const errorMessageElement = await this.driver.$('.error .response');
         await errorMessageElement.waitForDisplayed({ timeout: 10000 });
     
@@ -95,5 +95,86 @@ class TagsPage {
         assert.strictEqual(errorMessageText, mensaje, 'El mensaje de error no es el esperado.');
         
     }
+
+    async expandMetaData() {
+
+        let element = await this.driver.$('.gh-btn.gh-btn-expand');
+        await element.click();
+    }
+
+    async enterMetaTitle(titulo) {
+        
+        let element = await this.driver.$('#meta-title');
+        await element.waitForDisplayed(15000);
+        await element.setValue(titulo);
+    }
+
+    async enterMetaDescripcion(descripcion) {
+
+        let element = await this.driver.$('#meta-description');
+        await element.waitForDisplayed(15000);
+        await element.setValue(descripcion);
+    }
+
+    async enterMetaUrl(url) {
+        let element = await this.driver.$('#canonical-url');
+        await element.waitForDisplayed(15000);
+        await element.setValue(url);
+    }
+
+
+    async crearTagsMetaData(name = '', descripcion = '', slug = '') {
+        
+        //lets grab a random index
+        const randomIndex = Math.floor(Math.random() * this.testData.length);
+    
+        
+        let nameNote;
+
+        if ( name === "vacio" || name === "warning") 
+            nameNote = name === "vacio" ? "" :  this.testData[randomIndex].string_peligrosos;
+        else
+            nameNote = this.testData[randomIndex].name;
+
+
+        let textNote;
+
+        if ( descripcion === "max 500") 
+            textNote = this.testData[randomIndex].descripción_max;
+        else
+            textNote = this.testData[randomIndex].descripción;
+
+        let slugNote;
+
+            if ( slug === "max 191") 
+                slugNote = this.testData[randomIndex].descripción_max;
+            else
+                slugNote = this.testData[randomIndex].slug;
+
+        let titleMeta;
+        titleMeta = this.testData[randomIndex].titulo_meta;
+        let descripcionMeta;
+        descripcionMeta = this.testData[randomIndex].descripcion_meta;
+        let urlMeta;
+        urlMeta = this.testData[randomIndex].url;
+    
+        try {
+            
+            await this.enterName(nameNote); 
+            await this.slugName(slugNote); 
+            await this.enterMemberNote(textNote); 
+            await this.enterMetaTitle(titleMeta); 
+            await this.enterMetaDescripcion(descripcionMeta); 
+            await this.enterMetaUrl(urlMeta); 
+        } catch (error) {
+            console.error("Error en la creación de tags:", error);
+            throw error; 
+        }
+        
+        
+    }
+
+
+
 }
 module.exports = TagsPage;

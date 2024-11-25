52e1a516795e6758a7d2411009b322a2be36604e
Stevenhr <estevenhr@hotmail.com>
2024-11-24 20:29:49 -0500

    Feat(EP-023-DP): En editar del tag y en Meta Data, con datos con mas de 300 catracteres en Meta title
    En editar del tag y en Meta Data, con datos con mas de 300 catracteres en Meta title, debe presetnar el mensaje:'Meta Title cannot be longer than 300 characters.'

--- kraken/features/web/step_definitions/tagsSteps.js
+++ kraken/features/web/step_definitions/tagsSteps.js
@@ -234,3 +234,11 @@ When('I fetch data from Mockaroo API {string} tags meta data edit vacio title', 
     await tagsPage.getTestDataSet(apiUrl, 'GET');
     await tagsPage.editarTagsMetaData('otro','otro','','vacio','');  
 });
+
+
+When('I fetch data from Mockaroo API {string} tags meta data edit not allowed max 300 meta title', async function (apiUrl) {
+    
+    const tagsPage = new TagsPage(this.driver);
+    await tagsPage.getTestDataSet(apiUrl, 'GET');
+    await tagsPage.editarTagsMetaData('otro','otro','','max 300','');  
+});

c2512b6589836a4d86cb024a431f127380f2b8e4
Jhon Andres Parra R <ja.parrar12@uniandes.edu.co>
2024-11-24 19:21:48 -0500

    Generación de datos para el escenario 7 Cypress

--- cypress/cypress/support/utils/api-mockaroo.js
+++ cypress/cypress/support/utils/api-mockaroo.js
@@ -1,33 +1,48 @@
 const baseUrlMockaroo = Cypress.env("apirUrlMockaroo");
 const apiKeyMockaroo = Cypress.env("apiKeyMockaroo");
 
-
 export class ApiMockaroo {
-  
   static dataWrongRegisterUser() {
-    return cy.request({
-      method: "GET",
-      url: `${baseUrlMockaroo}/user_register.json?key=${apiKeyMockaroo}`,
-    }).then((response) => {
-      return response.body;
-    });
+    return cy
+      .request({
+        method: "GET",
+        url: `${baseUrlMockaroo}/user_register.json?key=${apiKeyMockaroo}`,
+      })
+      .then((response) => {
+        return response.body;
+      });
   }
 
   static dataWrongPasswordRegisterUser() {
-    return cy.request({
-      method: "GET",
-      url: `${baseUrlMockaroo}/user_register_wrong_password.json?key=${apiKeyMockaroo}`,
-    }).then((response) => {
-      return response.body;
-    })
+    return cy
+      .request({
+        method: "GET",
+        url: `${baseUrlMockaroo}/user_register_wrong_password.json?key=${apiKeyMockaroo}`,
+      })
+      .then((response) => {
+        return response.body;
+      });
   }
 
   static dataEmptyCreatePage() {
-    return cy.request({
-      method: "GET",
-      url: `${baseUrlMockaroo}/post_empty_title.json?key=${apiKeyMockaroo}`,
-    }).then((response) => {
-      return response.body;
-    })
+    return cy
+      .request({
+        method: "GET",
+        url: `${baseUrlMockaroo}/post_empty_title.json?key=${apiKeyMockaroo}`,
+      })
+      .then((response) => {
+        return response.body;
+      });
+  }
+
+  static dataCreateTag() {
+    return cy
+      .request({
+        method: "GET",
+        url: `${baseUrlMockaroo}/create_tag.json?key=${apiKeyMockaroo}`,
+      })
+      .then((response) => {
+        return response.body;
+      });
   }
 }
